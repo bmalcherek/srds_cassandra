23c6b8f6d92869ace50fc91272cad97bc7acd924
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2022-01-14 19:48:22 +0100

    basic varsion works

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"github.com/gocql/gocql"
 	"github.com/scylladb/gocqlx/v2"
 	"github.com/scylladb/gocqlx/v2/qb"
-	"github.com/scylladb/gocqlx/v2/table"
 )
 
 const (
@@ -17,12 +16,6 @@ const (
 	sectorRowCount int = 50
 	rowSeatCount   int = 100
 )
-
-var gameMetadata = table.Metadata{
-	Name:    "game",
-	Columns: []string{"game_id", "game_date", "game_team1", "game_team2", "stadium_name", "capacity"},
-	PartKey: []string{"game_id"},
-}
 
 func createGame(session *gocqlx.Session, date int64, capacity int, stadiumName, team1, team2 string) {
 	gameId, err := gocql.RandomUUID()
@@ -132,6 +125,7 @@ func reserveRandomSeat(session *gocqlx.Session, id int) {
 
 		checkSeat := models.GameReservation{
 			GameId: seatToReserve.GameId,
+			SeatId: seatToReserve.SeatId,
 		}
 		q = session.Query(models.GameReservations.Get()).BindStruct(checkSeat)
 		if err := q.GetRelease(&checkSeat); err != nil {
@@ -142,15 +136,87 @@ func reserveRandomSeat(session *gocqlx.Session, id int) {
 			fmt.Println("ERROROROROROROR")
 		}
 
-		fmt.Println(game, len(seats), len(emptySeats))
+		// fmt.Println(game, len(seats), len(emptySeats))
+	}
+}
+
+func initTables(session *gocqlx.Session) {
+	err := session.ExecStmt(`CREATE KEYSPACE IF NOT EXISTS tickets
+		WITH REPLICATION = {
+			'class': 'SimpleStrategy',
+			'replication_factor': 2
+	};`)
+	if err != nil {
+		panic(err)
+	}
+
+	err = session.ExecStmt(`CREATE TABLE IF NOT EXISTS tickets.games_by_stadiums (
+		game_id uuid,
+		game_date timestamp,
+		game_team1 text,
+		game_team2 text,
+		stadium_name text,
+		capacity int,
+		PRIMARY KEY (stadium_name, game_id)
+	);`)
+	if err != nil {
+		panic(err)
+	}
+
+	err = session.ExecStmt(`CREATE TABLE IF NOT EXISTS tickets.games (
+		game_id uuid,
+		game_date timestamp,
+		game_team1 text,
+		game_team2 text,
+		stadium_name text,
+		capacity int,
+		PRIMARY KEY (game_id)
+	);`)
+	if err != nil {
+		panic(err)
+	}
+
+	err = session.ExecStmt(`CREATE TABLE IF NOT EXISTS tickets.game_reservations (
+		game_id uuid,
+		seat_id text,
+		seat_owner text,
+		seat_price int,
+		seat_discount text,
+		PRIMARY KEY (game_id, seat_id)
+	);`)
+	if err != nil {
+		panic(err)
+	}
+
+	err = session.ExecStmt("TRUNCATE tickets.games;")
+	if err != nil {
+		panic(err)
+	}
+
+	err = session.ExecStmt("TRUNCATE tickets.games_by_stadiums;")
+	if err != nil {
+		panic(err)
+	}
+
+	err = session.ExecStmt("TRUNCATE tickets.game_reservations;")
+	if err != nil {
+		panic(err)
 	}
 }
 
 func main() {
 	cluster := gocql.NewCluster("127.0.0.1")
-	cluster.Keyspace = "test"
 
 	session, err := gocqlx.WrapSession(cluster.CreateSession())
+	if err != nil {
+		panic(err)
+	}
+
+	initTables(&session)
+	session.Close()
+
+	cluster.Keyspace = "tickets"
+	session, err = gocqlx.WrapSession(cluster.CreateSession())
 	if err != nil {
 		panic(err)
 	}
